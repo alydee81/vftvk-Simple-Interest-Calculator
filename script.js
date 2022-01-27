919b124e8a7b270e65d85632cd88c8d18cf67c6e
Alysia <alysiadelancey@gmail.com>
2022-01-27 17:11:40 +0000

    adding style and script

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").nodeValue;
+    if(principal<1){
+         alert("Enter a positive number");
+         return false;
+         console.log(principal)
+
+}
+    var rate = document.getElementById("rate").nodeValue;
+    console.log(rate);
+    var years = document.getElementById("years");
+    console.log(years);
+    var interest = principal*years*rate/100;
+    console.log(interest);
+    var year = new Date().getFullYear()+parseInt(years);
+    console.log(year);
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").nodeValue;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
